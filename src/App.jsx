0c75a6b8cc8bfdde4f559747a6b77a2299784a57
Aman khan <amaanwaseem24@hotmail.com>
2025-10-28 19:52:12 +0500

    Refactor app structure: add Navbar and Hero components, implement ComingSoon section, and update styles for better layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
 import { useState } from "react";
 import { ScrollTrigger } from "gsap/all";
 import { gsap } from "gsap/gsap-core";
+import Hero from "./sections/Hero";
+import Navbar from "./sections/Navbar";
 gsap.registerPlugin(ScrollTrigger);
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <>
-      <h1 className="text-9xl text-blue-600">ASDSA</h1>
-    </>
+    <main>
+      <Navbar />
+      <Hero />
+    </main>
   );
 }
 
---
+++ src/sections/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <nav>
+      <img src="/images/nav-logo.svg" alt="" className="scale-90" />
+      <img src="/images/menu.svg" className="w-10" alt="" />
+    </nav>
+  );
+};
+
+export default Navbar;
